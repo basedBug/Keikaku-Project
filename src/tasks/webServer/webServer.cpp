4b122da281ee0e37ea7134b71ffdeb1534f98e86
basedEnjoyer <nothanks@hotmail.com>
2025-08-10 08:28:37 -0700

    feat: add 404 not found page

--- src/tasks/webServer/webServer.cpp
+++ src/tasks/webServer/webServer.cpp
@@ -142,7 +142,7 @@ void initializeWebServer()
     
     server.onNotFound([](AsyncWebServerRequest* request)
 	{
-		request->send(404, "text/plain", "Not found");
+		request->send(LittleFS, "/not_found.html", "text/html");
 	});
 }
 
